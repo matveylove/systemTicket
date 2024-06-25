82aa34fe9e06f6b9b8fb77d4215f076d1d667961
Matvey <matveyka.bryantsev@mail.ru>
2024-06-25 19:11:33 +0700

    first commit

---
+++ src/data/links.js
@@ -0,0 +1,13 @@
+const startName = '/fe-48-diplom';
+
+const links = {
+   main: startName,
+   trains: `${startName}/trains`,
+   seats: `${startName}/seats`,
+   passengers: `${startName}/passengers`,
+   paymentOptions: `${startName}/paymentOptions`,
+   confirmOrder: `${startName}/confirmOrder`,
+   success: `${startName}/success`,
+};
+
+export default links;
